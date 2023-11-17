7862f68ff916f248c83135c3fb71b2d8a7d97e78
Mustafa <secure-admin@indicatortester.com>
2023-11-17 13:57:14 +0300

    Use Yahoo finance to fetch candles

--- scripts/candles-fetcher.py
+++ scripts/candles-fetcher.py
@@ -1,10 +1,8 @@
-import boto3
-import finnhub
-import time
-import json
 import os
-import calendar
-from datetime import datetime, timedelta, timezone
+import boto3
+import time
+import yfinance as yf
+import pandas as pd
 
 def getSupportedSymbols():
     dynamodb = boto3.client('dynamodb', region_name='eu-west-1')
@@ -25,70 +23,17 @@ def getSupportedSymbols():
     except Exception as e:
         return []
 
-def getYesterdayEpoch():
-    current_time_utc = datetime.utcnow()
-    yesterday_utc = current_time_utc - timedelta(days=1)
-    yesterday_start_of_day_utc = yesterday_utc.replace(hour=0, minute=0, second=0, microsecond=0)
-    return int(yesterday_start_of_day_utc.replace(tzinfo=timezone.utc).timestamp())
-
-def generate_monthly_date_pairs(start_date, end_date):
-    date_pairs = []
-    current_date = start_date
-    while current_date <= end_date:
-        last_day_of_month = calendar.monthrange(current_date.year, current_date.month)[1]
-        end_of_month = current_date.replace(day=last_day_of_month, hour=23, minute=59, second=59, tzinfo=timezone.utc)
-        date_pairs.append((int(current_date.timestamp()), int(end_of_month.timestamp())))
-        current_date = end_of_month + timedelta(seconds=1)
-    return date_pairs
-
-def fetchCandlesData(symbols):
-    pairs = [(1262304000, 1735689600)]
-    data = {}
+def main():
+    symbols = getSupportedSymbols()
     for symbol in symbols:
-        data[symbol['symbol']['S']] = {}
-        data[symbol['symbol']['S']]['t'] = []
-        data[symbol['symbol']['S']]['o'] = []
-        data[symbol['symbol']['S']]['h'] = []
-        data[symbol['symbol']['S']]['l'] = []
-        data[symbol['symbol']['S']]['c'] = []
-        data[symbol['symbol']['S']]['v'] = []
-        for s, e in pairs:
-            time.sleep(10)
-            print(f"Fetching data for symbol {symbol['symbol']['S']} from {s} to {e}")
-            finnhub_client = finnhub.Client(api_key="cl2a54hr01qinfqol99gcl2a54hr01qinfqol9a0")
-            gotData = False
-            retries = 3
-            while gotData == False and retries != 0:
-                response = finnhub_client.crypto_candles(symbol['exchangeSymbol']['S'], 'D', s, e)
-                if 't' in response and len(response['t']) != 0:
-                    break
-                retries = retries - 1
-                time.sleep(10)
-            if response['s'] == 'no_data':
-                continue
-            data[symbol['symbol']['S']]['t'].extend(response['t'])
-            data[symbol['symbol']['S']]['o'].extend(response['o'])
-            data[symbol['symbol']['S']]['h'].extend(response['h'])
-            data[symbol['symbol']['S']]['l'].extend(response['l'])
-            data[symbol['symbol']['S']]['c'].extend(response['c'])
-            data[symbol['symbol']['S']]['v'].extend(response['v'])
-            print(f"The number of candles -> {len(data[symbol['symbol']['S']]['t'])}")
-    return data
-
-def writeData(data):
-    formattedData = {}
-    for key, value in data.items():
+        time.sleep(30)
+        data = yf.download(symbol['exchangeSymbol']['S'], start="2000-01-01", end="2025-01-01")
         home = os.path.expanduser("~")
         path = os.path.join(home, 'IndicatorTester/candles')
         if not os.path.exists(path):
             os.makedirs(path)
-        with open(f"{path}/{key}.json", 'w') as json_file:
-            json.dump(value, json_file)
-
-def main():
-    symbols = getSupportedSymbols()
-    symbolsData = fetchCandlesData(symbols)
-    writeData(symbolsData)
+        with open(f"{path}/{symbol['symbol']['S']}.csv", 'w') as file:
+            file.write(pd.DataFrame(data).to_csv())
 
 if __name__ == "__main__":
     main()
