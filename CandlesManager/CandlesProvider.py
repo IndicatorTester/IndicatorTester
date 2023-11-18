a21e2fe314d5379ff75dfcf28a08753768d424e6
Mustafa <secure-admin@indicatortester.com>
2023-11-18 10:12:41 +0300

    Add getting data by interval in CandlesProvider

--- CandlesManager/CandlesProvider.py
+++ CandlesManager/CandlesProvider.py
@@ -1,11 +1,12 @@
-from pathlib import Path
-from os.path import expanduser
 import pandas as pd
+from os.path import expanduser
 
 class CandlesProvider:
     @classmethod
-    def getCandles(cls, symbol) -> pd.DataFrame:
+    def getCandles(cls, symbol, startDate = '2000-01-01', endDate = '2029-12-31') -> pd.DataFrame:
         dataFilePath =  expanduser(f"~/IndicatorTester/candles/{symbol}.csv")
         with open(dataFilePath, 'r') as file:
             data = pd.read_csv(file)
-        return data
+        data['Date'] = pd.to_datetime(data['Date'])
+        data.set_index('Date', inplace=True)
+        return data.loc[pd.to_datetime(startDate):pd.to_datetime(endDate)]
