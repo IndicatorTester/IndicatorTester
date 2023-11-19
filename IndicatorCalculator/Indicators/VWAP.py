26348b6d7b18624bed530463f7dc1dd0859133c2
Mustafa <secure-admin@indicatortester.com>
2023-11-19 11:33:31 +0300

    Add default value to VWAP indicator

--- IndicatorCalculator/Indicators/VWAP.py
+++ IndicatorCalculator/Indicators/VWAP.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Volume Weighted Average Price
-def vwap(data: pd.DataFrame, period):
+def vwap(data: pd.DataFrame, period = 20):
     volume = data['Volume']
     price = data['Close']
     vwap = (volume * price).rolling(period).sum() / volume.rolling(period).sum()
