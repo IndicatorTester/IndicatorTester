e90920192eb704a3796b42881b4bd8d46fb98568
Mustafa <secure-admin@indicatortester.com>
2023-12-21 17:19:29 +0300

    Add validation to /preOrder api

--- activities/AddPreOrderActivity.py
+++ activities/AddPreOrderActivity.py
@@ -12,6 +12,11 @@ class AddPreOrderActivity:
         self._handler = AddPreOrderHandler.instance()
 
     def act(self, request: AddPreOrderRequest):
+        if request.ip is None:
+            raise HTTPException(status_code = 403, detail = 'IP address can\'t be null')
+        if request.email is None:
+            raise HTTPException(status_code = 403, detail = 'Email can\'t be null')
+
         try:
             self._handler.handle(request)
         except Exception as e:
--- models/AddPreOrderRequest.py
+++ models/AddPreOrderRequest.py
@@ -1,5 +1,5 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, EmailStr, Field
 
 class AddPreOrderRequest(BaseModel):
     ip: str = Field(None)
-    email: str = Field(None)
+    email: EmailStr = Field(None)
