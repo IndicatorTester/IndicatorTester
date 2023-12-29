ed245970c6d703135bb2f75ddabbbc4dd76fffd1
Mustafa <secure-admin@indicatortester.com>
2023-12-29 18:12:55 +0300

    Change AutoTrader indicator

--- tools/AutoTrader.py
+++ tools/AutoTrader.py
@@ -9,7 +9,7 @@ import logging
 BUY_SIGNAL = 'Buy'
 SELL_SIGNAL = 'Sell'
 TRADING_INTERVAL = '1h'
-TRADING_INDICATOR = 'sma(close, 3) > sma(open, 3)'
+TRADING_INDICATOR = 'sma(close, 2) > sma(open, 2)'
 DATA_PROVIDER_KEY = 'e21a1ce91bfc46d79fa61834cfcedff3'
 SYMBOLS = ['DOGE/USD', 'MATIC/USD', 'FIL/USD', 'RUNE/USD', 'XRP/USD', 'OP/USD']
 
